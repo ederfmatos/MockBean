eec201b208ce621f4c388b8b361034bf31001434
Eder Matos <ederfmatos@gmail.com>
2020-07-06 09:01:38 -0300

    FIX: Fixed utility map declaration ReflectionUtils

--- src/main/java/com/ederfmatos/mockbean/random/utils/ReflectionUtils.java
+++ src/main/java/com/ederfmatos/mockbean/random/utils/ReflectionUtils.java
@@ -1,19 +1,24 @@
 package com.ederfmatos.mockbean.random.utils;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public final class ReflectionUtils {
 
-    private static final Map<Class<?>, Class<?>> wrapperPrimitiveClass = Map.of(
-            char.class, Character.class,
-            int.class, Integer.class,
-            boolean.class, Boolean.class,
-            byte.class, Byte.class,
-            double.class, Double.class,
-            float.class, Float.class,
-            long.class, Long.class,
-            short.class, Short.class
-    );
+    private static final Map<Class<?>, Class<?>> wrapperPrimitiveClass;
+
+    static {
+        wrapperPrimitiveClass = new HashMap<>();
+
+        wrapperPrimitiveClass.put(char.class, Character.class);
+        wrapperPrimitiveClass.put(int.class, Integer.class);
+        wrapperPrimitiveClass.put(boolean.class, Boolean.class);
+        wrapperPrimitiveClass.put(byte.class, Byte.class);
+        wrapperPrimitiveClass.put(double.class, Double.class);
+        wrapperPrimitiveClass.put(float.class, Float.class);
+        wrapperPrimitiveClass.put(long.class, Long.class);
+        wrapperPrimitiveClass.put(short.class, Short.class);
+    }
 
     private ReflectionUtils() {
     }
